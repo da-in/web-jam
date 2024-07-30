3c5c81f45f1b36133e2b5ca7c306c3cf49208e78
da-in <talown@naver.com>
2024-07-30 13:39:54 +0900

    refactor (07.30)

--- 0730_memo/index.ts
+++ 0730_memo/index.ts
@@ -1,20 +1,17 @@
-const cache  = new Map<string, any>()
-
-const hasKey = (key: string) => {
-    return cache.has(key)
-}
-
-const getKey = (...args) => {
-    return JSON.stringify(args)
-}
 
 export const memo = (fn) => {
-    const _fn = (...args) => {
+    const cache  = new Map<string, any>()
+    const hasKey = (key: string) => {
+        return cache.has(key)
+    }
+    const getKey = (...args) => {
+        return JSON.stringify(args)
+    }
+    return (...args) => {
         const key = getKey(args)
         if(!hasKey(key)){
             cache.set(key, fn(...args))
         }
         return cache.get(key)
     }
-    return _fn
 }
